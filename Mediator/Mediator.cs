c23eeb1715d78906b6b308717ed69bb07a202a90
Danihmn <daniel.bezerra.mult@gmail.com>
2026-07-25 13:16:09 -0300

    Adiciona classe do Mediator, contendo lógica de combinação do Handler a partir da Request enviada

---
+++ Mediator/Mediator.cs
@@ -0,0 +1,25 @@
+﻿using Mediator.Abstractions;
+
+namespace Mediator;
+
+public class Mediator(IServiceProvider serviceProvider) : IMediator
+{
+    public async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request,
+        CancellationToken cancellationToken = default)
+    {
+        var requestType = request.GetType();
+        var handlerType = typeof(IHandler<,>).MakeGenericType(requestType, typeof(TResponse));
+
+        var handler = serviceProvider.GetService(handlerType) ??
+                      throw new InvalidOperationException($"Handler nos found for {requestType}");
+        var method = handlerType.GetMethod("HandleAsync") ??
+                     throw new InvalidOperationException($"Method 'HandleAsync' not found in {handlerType}");
+
+        var result = method.Invoke(handler, [request, cancellationToken]);
+
+        if (result is not Task<TResponse> task)
+            throw new InvalidOperationException("Method returns unexpected type");
+
+        return await task;
+    }
+}
