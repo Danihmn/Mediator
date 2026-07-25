9bce4726f8bbf61877b33811b7bff93df5684e77
Danihmn <daniel.bezerra.mult@gmail.com>
2026-07-25 13:37:10 -0300

    Adiciona docstrings

--- Mediator.Abstractions/IMediator.cs
+++ Mediator.Abstractions/IMediator.cs
@@ -2,5 +2,12 @@
 
 public interface IMediator
 {
+    /// <summary>
+    ///  Define um contrato responsável por mediar o envio para o Handler e retorno para quem mandou a request
+    /// </summary>
+    /// <param name="request">Request enviada</param>
+    /// <param name="cancellationToken">Token de cancelamento</param>
+    /// <typeparam name="TResponse">Response devolvida para quem enviou a request</typeparam>
+    /// <returns>Response para quem enviou a request</returns>
     Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
 }
--- Mediator/Mediator.cs
+++ Mediator/Mediator.cs
@@ -8,13 +8,19 @@ public class Mediator(IServiceProvider serviceProvider) : IMediator
         CancellationToken cancellationToken = default)
     {
         var requestType = request.GetType();
+
+        // Encontra o tipo do Handler dinamicamente, combinando o tipo da request e da response, em runtime
         var handlerType = typeof(IHandler<,>).MakeGenericType(requestType, typeof(TResponse));
 
+        // Resolve a instância do Handler necessário no DI, buscando o pelo seu tipo
         var handler = serviceProvider.GetService(handlerType) ??
                       throw new InvalidOperationException($"Handler nos found for {requestType}");
+
+        // Busca o método do Handler, em runtime
         var method = handlerType.GetMethod("HandleAsync") ??
                      throw new InvalidOperationException($"Method 'HandleAsync' not found in {handlerType}");
 
+        // Chama o método, passando nos parâmetros a request e o cancellationToken
         var result = method.Invoke(handler, [request, cancellationToken]);
 
         if (result is not Task<TResponse> task)
